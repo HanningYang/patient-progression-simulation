5a5f025b23f0ff51d03895a07fba8f182bdbec21
Hanning YANG <114662102+HanningYang@users.noreply.github.com>
2024-10-28 13:05:28 +0100

    Update clinician_app.py

--- clinician_app.py
+++ clinician_app.py
@@ -261,7 +261,7 @@ std_C_se = st.sidebar.number_input('Severe CRP Variability', value=55.0)
 
 # Parameters to adjust
 st.sidebar.markdown("## Growth/Deacay Rate of Different Biomarkers")
-st.sidebar.markdown("#### A positive value for the growth/decay rate indicates that the biomarker tends to increase (growth) over time, while a negative value indicates a decrease (decay) over time.")
+st.sidebar.markdown("A positive value for the growth/decay rate indicates that the biomarker tends to increase (growth) over time, while a negative value indicates a decrease (decay) over time.")
 r_C1 = st.sidebar.slider('CRP', min_value=-1.0, max_value=1.0, value=0.04, step=0.01, format="%.2f")
 r_H1 = st.sidebar.slider('Haemoglobin', min_value=-1.0, max_value=1.0, value=-0.12, step=0.01, format="%.2f")
 r_W1 = st.sidebar.slider('BMI', min_value=-1.0, max_value=1.0, value=-0.05, step=0.01, format="%.2f")
@@ -269,7 +269,7 @@ r_A1 = st.sidebar.slider('Albumin', min_value=-1.0, max_value=1.0, value=-0.05, 
 r_I1 = st.sidebar.slider('Iron', min_value=-1.0, max_value=1.0, value=-0.14, step=0.01, format="%.2f")
 
 st.sidebar.markdown("## Difference in Growth/Decay Rate for Severe Group Relative to Intermediate Group to Differentiate Patient Severity")
-st.sidebar.markdown("#### A positive correlation means that as one variable increases or decreases, the other tends to change in the same direction. A negative correlation means that as one variable increases, the other tends to decrease, and vice versa. The larger the absolute value, the stronger the correlation.")
+st.sidebar.markdown("A positive correlation means that as one variable increases or decreases, the other tends to change in the same direction. A negative correlation means that as one variable increases, the other tends to decrease, and vice versa. The larger the absolute value, the stronger the correlation.")
 
 delta1 = st.sidebar.slider('CRP Growth/Decay Difference', min_value=-1.0, max_value=1.0, value=0.16, step=0.01, format="%.2f")
 
@@ -283,7 +283,7 @@ theta_HI1 = st.sidebar.slider('Haemoglobin and Iron', min_value=-1.0, max_value=
 
 
 st.sidebar.markdown("## Variability")
-st.sidebar.markdown("#### If you choose not to add variability, you can leave the variability level alone.")
+st.sidebar.markdown("If you choose not to add variability, you can leave the variability level alone.")
 add_noise = st.sidebar.checkbox('Add Variability', True)
 noise_level = st.sidebar.number_input('Variability Level', min_value=0.0, max_value=2.0, value=0.5, step=0.1, format="%.1f")
 
